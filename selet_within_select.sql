ccdcc5f6a6f0c747768af097f69674e88ca2c7aa
0175077 <43737753+0175077@users.noreply.github.com>
2020-11-17 11:20:26 +0900

    Update selet_within_select.sql

--- selet_within_select.sql
+++ selet_within_select.sql
@@ -1,6 +1,19 @@
 #1
-
 SELECT name FROM world
   WHERE population >
      (SELECT population FROM world
       WHERE name='Russia')
+
+#2
+select name from world
+where gdp/population > (select gdp/population from world
+where name = 'united kingdom')
+and continent = 'europe'
+
+#3
+select name, continent from world
+where continent in (select continent from world
+where name in ('argentina','australia'))
+order by name
+
+#4
